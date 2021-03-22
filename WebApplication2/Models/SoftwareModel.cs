b07dd8f790d69bfb3afb1c8b69412e2a1775f427
softman <gharibehdlife@gmail.com>
2021-03-22 13:11:11 +0430

    111

---
+++ WebApplication2/Models/SoftwareModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class SoftwareModel
+    {
+      
+
+        public string  Name { get; set; }
+        public string  Family { get; set; }
+        public int Namber { get; set; }
+
+
+
+    }
+}
---
+++ WebApplication2/ViewComponent/ViewComponent.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace MyProtfolio.ViewComponent
+{
+    public class SoftwareViewComponent : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+
+            var softwares = new List<SoftwareModel>
+            {
+                new SoftwareModel
+                {
+                    Name = "Sina",
+                    Family = "hosieni",
+                    Namber = 7,
+                },
+                new SoftwareModel
+                {
+                    Name = "mohammad",
+                    Family =" rezaee",
+                    Namber = 2,
+                }
+                /*farzad2609*/
+            };
+
+            return View("_ListView", softwares);
+
+        }
+
+    }
+}
