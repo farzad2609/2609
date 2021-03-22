4959d60455585c68a374b0ccf10f00fa2934c84d
softman <gharibehdlife@gmail.com>
2021-03-22 14:29:26 +0430

    1212

--- WebApplication2/Models/ListViewModel.cs
+++ WebApplication2/Models/ListViewModel.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit.Sdk;
 
 namespace WebApplication2.Models
 {
     public class ListViewModel
     {
-        public string Name { get; set; }
-         
-        public string Family { get; set; }
-
+            [Required(ErrorMessage = "این فیلد اجباری است")]
+            [MinLength(2, ErrorMessage = "اسم اشتباه است")]
+            public string Name { get; set; }
+            [Required(ErrorMessage = "این فیلد اجباری است")]
+            [EmailAddress(ErrorMessage = "فرمت ایمیل اشتباه است")]
+            public string Email { get; set; }
+            [Required(ErrorMessage = "این فیلد اجباری است")]
+            [MinLength(2, ErrorMessage = "پیام اشتباه است")]
+            public string Message { get; set; }
        
     }
 }
